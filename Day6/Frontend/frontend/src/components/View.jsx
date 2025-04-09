cc681e31bbb65e92670e6c62dabaae5b9eafc8c1
aryan5186 <aryanpanchal0412n@gmail.com>
2025-04-09 11:17:20 +0530

    add files

--- Day6/Frontend/frontend/src/components/View.jsx
+++ Day6/Frontend/frontend/src/components/View.jsx
@@ -1,34 +1,46 @@
-import React, { useState, useEffect } from 'react'
- import axios from 'axios'
- const View = () => {
-     const [users, setUsers] = useState([]);
-     useEffect(() => {
-         viewdata();
-     }, [])
-     const viewdata = async () => {
-         const res = await axios.get('http://localhost:9000/users');
-         const res = await axios.get('https://fsd-backend-sem-vi-ds-a.onrender.com/users');
-         console.log(res);
-         setUsers(res.data);
-     }
- 
-     return (
-         <div>
-             <h1>Registerd Users List</h1>
-             <table style={{ border: '2px solid red', backgroundColor: 'greenyellow', width: '100%' }}>
-                 <tr>
-                     <th>ID</th>
-                     <th>Name</th>
-                     <th>Age</th>
-                 </tr>
-                 {users.map((user) => (
-                     <tr key={user.id}>
-                         <td>{user.id}</td>
-                         <td>{user.name}</td>
-                         <td>{user.age}</td>
-                     </tr>
-                 ))}
-             </table>
-         </div>
-     )
- }
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const View = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    viewdata();
+  }, []);
+
+  const viewdata = async () => {
+    try {
+      const res = await axios.get('https://fsd-backend-sem-vi-ds-a.onrender.com/users');
+      console.log(res.data);
+      setUsers(res.data);
+    } catch (error) {
+      console.error('Error fetching users:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Registered Users List</h1>
+      <table style={{ border: '2px solid red', backgroundColor: 'greenyellow', width: '100%' }}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Age</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default View;
